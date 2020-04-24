512fd28c5a9269e0cdb3092c985aff0f5fe6c309
Diego Rocchini <roquinni@gmail.com>
2020-04-24 15:09:34 -0300

    recipe items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
-import { ServerComponent } from './server/server.component';
-import { ExerciseBasicsComponent } from './exercise-basics/exercise-basics.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ExerciseBasicsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
