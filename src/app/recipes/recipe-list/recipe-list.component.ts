99d603d014eaddc03ca0787c25db773130763873
Diego Rocchini <roquinni@gmail.com>
2020-04-24 22:53:25 -0300

    recipe list plus item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,9 +14,9 @@ export class RecipeListComponent implements OnInit {
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
     ),
     new Recipe(
-      'A test recipe',
-      'our beaultifull desc',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'Another test recipe',
+      'Your pretty desc',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
     ),
   ];
 
