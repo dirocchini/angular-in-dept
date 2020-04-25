fbf286b8a6114eeb29e86ec703285fb4b56f3e90
Diego Rocchini <roquinni@gmail.com>
2020-04-24 22:44:23 -0300

    recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'our beaultifull desc',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
     ),
+    new Recipe(
+      'A test recipe',
+      'our beaultifull desc',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
   ];
 
   constructor() {}
