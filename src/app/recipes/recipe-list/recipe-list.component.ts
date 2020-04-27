825d166395efd4c811f9036e3f187b59467f26b5
Diego Rocchini <roquinni@gmail.com>
2020-04-27 18:47:39 -0300

    recipe list and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+
   recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
@@ -23,4 +26,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  currentRecipe(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
