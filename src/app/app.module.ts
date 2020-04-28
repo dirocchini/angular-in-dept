918bdd49ebced6bd8cddaaf57521307989f8d5d7
Diego Rocchini <roquinni@gmail.com>
2020-04-28 13:07:12 -0300

    dropdown directive enhanced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
-import { DropdownDirective } from './shared/dropdown.directive.directive';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
