3e84a5e009346221f4e7d31835a6f518d2c89872
Diego Rocchini <roquinni@gmail.com>
2020-04-28 22:24:47 -0300

    shopping list service enhanced

--- src/app/_services/recipe.service.ts
+++ src/app/_services/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -8,16 +11,29 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'our beaultifull desc',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 3),
+        new Ingredient('Tomatoes', 1),
+        new Ingredient('Bread', 1),
+        new Ingredient('French Fries', 30),
+      ]
     ),
     new Recipe(
       'Another test recipe',
       'Your pretty desc',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [new Ingredient('Shrimp', 10), new Ingredient('Letuce', 5)]
     ),
   ];
 
+  constructor(private shoppingService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingService.addIngredients(ingredients);
   }
 }
